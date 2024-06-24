ad5addd58ea10b19e23b94d21bfc5e4b1c62d1f2
Joshua Jacobs <joshua.jacobs126@gmail.com>
2024-06-24 08:50:56 -0500

    Header bar isn't as sticky now

--- luxurysite.client/src/Components/HeaderBar.jsx
+++ luxurysite.client/src/Components/HeaderBar.jsx
@@ -2,10 +2,26 @@ import menuIcon from '../assets/hamburger-menu.svg';
 import searchIcon from '../assets/search-icon.svg';
 import bagIcon from '../assets/bag-icon.svg';
 import styles from './HeaderBar.module.css'
+import { useState, useEffect } from 'react';
 
 function HeaderBar() {
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            setIsVisible(scrollPosition < 805);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className={styles.mainNavBar }>
+        <nav className={`${styles.mainNavBar} ${isVisible ? "" : styles.hideNav}`} aria-hidden={!isVisible}>
             <div className={styles.siteHeaderLeft} >
                 <ul role="list">
                     <li><a href="#0">Women</a></li>
